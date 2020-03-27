821979cc20653797c372dc723faf6cc063630d23
robert1003 <b07902047@ntu.edu.tw>
2020-03-27 13:40:28 +0800

    modify download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-early_ckpt=https://github.com/robert1003/ADL-Assignments/releases/download/hw1-early/model.ckpt
-early_word2vec=https://github.com/robert1003/ADL-Assignments/releases/download/hw1-early/word2vec.pickle
+early_zip=140.112.90.197:9763/early.zip
 
-wget "${early_ckpt}" -O ./early/model.ckpt
-wget "${early_word2vec}" -O ./word2vec.pickle
+wget "${early_zip}" -O ./temp.zip
+unzip temp.zip
+mv model.ckpt early/model.ckpt
+rm temp.zip
