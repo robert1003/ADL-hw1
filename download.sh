790ab8af18980890f4d800a4d857b41d226aed08
robert1003 <b07902047@ntu.edu.tw>
2020-03-28 09:18:03 +0800

    Fix download.sh ./ temp -> ./temp

--- download.sh
+++ download.sh
@@ -4,16 +4,16 @@ word2vec=140.112.90.197:9763/word2vec.zip
 early=140.112.90.197:9763/early.zip
 seq2seq=140.112.90.197:9763/seq2seq.zip
 
-wget "${word2vec}" -O ./ temp.zip
+wget "${word2vec}" -O ./temp.zip
 unzip temp.zip
 rm temp.zip
 
-#wget "${early}" -O ./ temp.zip
+#wget "${early}" -O ./temp.zip
 #unzip temp.zip
 #mv model.ckpt early/model.ckpt
 #rm temp.zip
 
-wget "${seq2seq}" -O ./ temp.zip
+wget "${seq2seq}" -O ./temp.zip
 unzip temp.zip
 mv model.ckpt seq2seq/model.ckpt
 rm temp.zip
