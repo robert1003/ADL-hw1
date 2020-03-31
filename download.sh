cf17432298c20c03987a8b215bba6ac296dbf4b3
robert1003 <b07902047@ntu.edu.tw>
2020-03-31 15:05:59 +0800

    fix link

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-word2vec=140.112.90.197:9763/word2vec.zip
-early=140.112.90.197:9763/early.zip
-extractive=140.112.90.197:9763/extractive.zip
-seq2seq=140.112.90.197:9763/seq2seq.zip
+word2vec=140.112.90.197:9763/hw1/word2vec.zip
+early=140.112.90.197:9763/hw1/early.zip
+extractive=140.112.90.197:9763/hw1/extractive.zip
+seq2seq=140.112.90.197:9763/hw1/seq2seq.zip
 
 wget "${word2vec}" -O ./temp.zip
 unzip temp.zip
