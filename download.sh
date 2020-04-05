f1a5e8b2eed54fb836014997d5250ca612eb156c
robert1003 <b07902047@ntu.edu.tw>
2020-04-05 22:54:44 +0800

    update

--- download.sh
+++ download.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-word2vec=140.112.90.197:9763/hw1/word2vec.zip
 early=140.112.90.197:9763/hw1/early.zip
 extractive=140.112.90.197:9763/hw1/extractive.zip
 seq2seq=140.112.90.197:9763/hw1/seq2seq.zip
-
-wget "${word2vec}" -O ./temp.zip
-unzip temp.zip
-rm temp.zip
+attention=140.112.90.197:9763/hw1/attention.zip
 
 #wget "${early}" -O ./temp.zip
 #unzip temp.zip
@@ -16,10 +12,15 @@ rm temp.zip
 
 wget "${extractive}" -O ./temp.zip
 unzip temp.zip
-mv model_extractive_best_rogue1.ckpt extractive/model_extractive_best_rogue1.ckpt
+mv model_best_rouge1.ckpt extractive/model_best_rouge1.ckpt
 rm temp.zip
 
 wget "${seq2seq}" -O ./temp.zip
 unzip temp.zip
 mv model.ckpt seq2seq/model.ckpt
+rm temp.zip
+
+wget "${attention}" -O ./temp.zip
+unzip temp.zip
+mv model_best_rouge1.ckpt attention/model_best_rouge1.ckpt
 rm temp.zip
