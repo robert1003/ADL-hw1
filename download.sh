85d4af2966cc9b60c444ed969dc62186064919f7
robert1003 <b07902047@ntu.edu.tw>
2020-04-06 08:42:08 +0800

    fix

--- download.sh
+++ download.sh
@@ -12,15 +12,12 @@ attention=140.112.90.197:9763/hw1/attention.zip
 
 wget "${extractive}" -O ./temp.zip
 unzip temp.zip
-mv model_best_rouge1.ckpt extractive/model_best_rouge1.ckpt
 rm temp.zip
 
 wget "${seq2seq}" -O ./temp.zip
 unzip temp.zip
-mv model.ckpt seq2seq/model.ckpt
 rm temp.zip
 
 wget "${attention}" -O ./temp.zip
 unzip temp.zip
-mv model_best_rouge1.ckpt attention/model_best_rouge1.ckpt
 rm temp.zip
